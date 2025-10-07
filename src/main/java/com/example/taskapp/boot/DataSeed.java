c4094e9a25bdc30d9f6c302508db2c03661d3273
Alessandro_Lingua <ale.lingua93@gmail.com>
2025-10-07 17:44:18 +0200

    final init con db on file

--- src/main/java/com/example/taskapp/boot/DataSeed.java
+++ src/main/java/com/example/taskapp/boot/DataSeed.java
@@ -11,23 +11,11 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 @Slf4j
 class DataSeed implements CommandLineRunner {
-
     private final TaskRepository repo;
 
     @Override
     public void run(String... args) {
         if (repo.count() == 0) {
-            Task t1 = new Task();
-            t1.setTitle("Spring Boot");
-            t1.setDescription("API + JPA");
-
-            Task t2 = new Task();
-            t2.setTitle("Frontend AngularJS");
-            t2.setDescription("SPA semplice");
-
-            repo.save(t1);
-            repo.save(t2);
-
             log.info("Seed dati completato: {} task", repo.count());
         }
     }
